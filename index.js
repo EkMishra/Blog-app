8ead05723b55ce4d18a8c94b64d351d2f7f2c864
EkMishra <113630549+EkMishra@users.noreply.github.com>
2024-02-29 01:02:40 +0530

    Added Style

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 // array to store data
 var blog = [];
-
+var first =[{title: "Initial Post",content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus, tellus a porttitor porta, risus dolor mollis enim, et consequat lacus neque ac purus. Aenean sed malesuada elit, et facilisis tortor. Nunc eu leo urna. Sed id massa id sapien porttitor auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius diam non lorem interdum, pretium venenatis neque sagittis. Nullam at molestie nunc. Phasellus dignissim ac mi non luctus. Aenean eu tristique lorem. Ut mattis, lectus eget mollis scelerisque, eros est interdum turpis, placerat sodales nibh eros iaculis magna. Fusce blandit massa et quam hendrerit vehicula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi gravida lacus ligula, quis vestibulum mauris congue ut. Praesent pharetra arcu sem, ut vulputate erat ornare non. Praesent dictum risus dapibus magna euismod, ac rutrum eros tempor. Sed dapibus ac dolor quis malesuada. Proin lorem leo, facilisis sed sapien et, venenatis mollis lorem. Etiam sed odio quis mauris auctor mattis. Donec auctor bibendum dapibus. Duis volutpat et eros in dapibus. Vivamus viverra mauris nec felis rutrum dignissim eu eu nibh. Aliquam et lacus eget justo egestas malesuada non vitae dui. Ut sit amet purus in justo luctus eleifend. Maecenas nunc ligula, tempor sodales arcu nec, gravida pulvinar sapien. Quisque a mollis dui."}]
 // Constructor funtion  
 var idCounter = 0;
 function BlogMain(title,content){
@@ -33,7 +33,7 @@ app.use(express.urlencoded({extended:true}));
 // First response
 app.get("/",(req,res)=>{
     if(blog.length === 0){
-    res.render("index.ejs",{blog : [{title : "none", content : "none"}]});
+    res.render("index.ejs",{blog : first});
     }
     else{
         res.render("index.ejs",{blog : blog});
